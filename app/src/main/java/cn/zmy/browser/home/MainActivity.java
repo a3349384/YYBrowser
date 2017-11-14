fbb7e18ea537b5b8b135a156153e21b1f0a91ed1
zhoumy <as20102010>
2017-11-14 21:54:52 +0800

    新增：首页增加网址输入布局

--- app/src/main/java/cn/zmy/browser/home/MainActivity.java
+++ app/src/main/java/cn/zmy/browser/home/MainActivity.java
@@ -1,8 +1,9 @@
-package cn.zmy.browser;
+package cn.zmy.browser.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.zmy.browser.R;
 import cn.zmy.browser.widget.TemperatureView;
 
 public class MainActivity extends AppCompatActivity
