c076cd06162a0efa7dd0e18b41724d464725d65a
zhoumy <as20102010>
2017-11-15 16:29:39 +0800

    完善：SearchActivity禁用启动和退出动画

--- app/src/main/java/cn/zmy/browser/search/SearchActivity.java
+++ app/src/main/java/cn/zmy/browser/search/SearchActivity.java
@@ -1,9 +1,14 @@
 package cn.zmy.browser.search;
 
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+
+import com.jaeger.library.StatusBarUtil;
 
 import cn.zmy.browser.R;
 
@@ -17,6 +22,19 @@ public class SearchActivity extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            View decor = getWindow().getDecorView();
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        StatusBarUtil.setTransparent(this);
         DataBindingUtil.setContentView(this, R.layout.activity_search);
+    }
+
+    @Override
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(0,0);
     }
 }
