05bc5f20827013f35bef96bde54a0de2e8b4cd3d
zhoumy <as20102010>
2017-11-15 13:37:39 +0800

    新增：首页网页输入框增加点击监听事件

--- app/src/main/java/cn/zmy/browser/home/MainActivity.java
+++ app/src/main/java/cn/zmy/browser/home/MainActivity.java
@@ -1,19 +1,26 @@
 package cn.zmy.browser.home;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cn.zmy.browser.R;
+import cn.zmy.browser.databinding.ActivityMainBinding;
+import cn.zmy.browser.home.viewmodel.HomeInputUrlViewModel;
 import cn.zmy.browser.widget.TemperatureView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private HomeInputUrlViewModel mHomeInputUrlViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mHomeInputUrlViewModel = new HomeInputUrlViewModel();
+        binding.includeHomeInputUrl.setHomeInputUrlViewModel(mHomeInputUrlViewModel);
 
         TemperatureView temperatureView = findViewById(R.id.temperatureView);
         temperatureView.setTemperature(18);
---
+++ app/src/main/java/cn/zmy/browser/home/viewmodel/HomeInputUrlViewModel.java
@@ -0,0 +1,16 @@
+package cn.zmy.browser.home.viewmodel;
+
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by zmy on 2017/11/15.
+ */
+
+public class HomeInputUrlViewModel
+{
+    public void onInputClick()
+    {
+        Log.d("Click", "点击点击");
+    }
+}
