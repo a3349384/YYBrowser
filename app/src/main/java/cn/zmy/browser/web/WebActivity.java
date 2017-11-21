5aab85c60b5c66d27668d7fe7b458ba3b8fb29b4
zhoumy <as20102010>
2017-11-21 12:25:00 +0800

    新增：web页面titlebar，同时优化了布局和背景颜色绘制

--- app/src/main/java/cn/zmy/browser/web/WebActivity.java
+++ app/src/main/java/cn/zmy/browser/web/WebActivity.java
@@ -1,6 +1,11 @@
 package cn.zmy.browser.web;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import cn.zmy.browser.R;
 
 /**
  * Created by zmy on 2017/11/19.
@@ -8,5 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 
 public class WebActivity extends AppCompatActivity
 {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        DataBindingUtil.setContentView(this, R.layout.activity_web);
+    }
 }
